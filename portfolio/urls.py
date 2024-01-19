2def8ddc21bb65b052b9fc087c7ab4c942a77cf7
Maxzimmerman <115488694+Maxzimmerman@users.noreply.github.com>
2024-01-19 18:41:51 +0100

    init

---
+++ portfolio/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path("test", TemplateView.as_view(template_name="test"))
+]
---
+++ portfolio_max/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from portfolio_max.users.models import User
+from portfolio_max.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
