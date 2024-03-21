629d8be07f81f12512bac14188c0afd3abf85d65
Max Zimmermann <Max Zimmermann@DESKTOP-5OOBUIK>
2024-03-21 19:44:59 +0100

    gg

--- kurvendiskussion/views.py
+++ kurvendiskussion/views.py
@@ -10,34 +10,31 @@ from sympy.parsing.sympy_parser import parse_expr
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        request.calc = None
-        if 'calc' in request.session:
-            print('there')
-        else:
-            print('not there')
-        print(request.calc)
         return render(request, 'kurvendiskussion/home.html')
 
     def post(self, request, *args, **kwargs):
-        input_data = request.POST.get('function')
-        x_stelle = request.POST.get('x_stelle_for_tangente')
-        function = parse_expr(input_data.replace('^', '**'))
-        calc = Calc(function)
-        exponenten = calc.gebe_exponenten_zurück(function)
-        calc.bestimme_y_achsenabschnitt(function)
-        calc.bestimme_symmetrie(exponenten)
-        calc.bestimme_grenzwertverhalten(-100, 100, function)
-        calc.ermittle_ableitungen(function)
-        calc.ermittle_nullstellen(function)
-        calc.ermittle_extremstellen(calc.f1, function)
-        calc.ermittle_wendestellen()
-        calc.monotonie(-100, 100)
-        calc.krümmung(-100, 100)
-        if x_stelle:
-            calc.bestimme_tangente(float(x_stelle))
-
-        context = {'calc': calc}
-        return render(request, "kurvendiskussion/partials/result.html", context)
+        try:
+            input_data = request.POST.get('function')
+            x_stelle = request.POST.get('x_stelle_for_tangente')
+            function = parse_expr(input_data.replace('^', '**'))
+            calc = Calc(function)
+            exponenten = calc.gebe_exponenten_zurück(function)
+            calc.bestimme_y_achsenabschnitt(function)
+            calc.bestimme_symmetrie(exponenten)
+            calc.bestimme_grenzwertverhalten(-100, 100, function)
+            calc.ermittle_ableitungen(function)
+            calc.ermittle_nullstellen(function)
+            calc.ermittle_extremstellen(calc.f1, function)
+            calc.ermittle_wendestellen()
+            calc.monotonie(-100, 100)
+            calc.krümmung(-100, 100)
+            if x_stelle:
+                calc.bestimme_tangente(float(x_stelle))
+            context = {'calc': calc}
+            return render(request, "kurvendiskussion/partials/result.html", context)
+        except:
+            context = {'fehler': 'Ups die Eingabe ist Fehlerhaft. Versuche es erneut.'}
+            return render(request, "kurvendiskussion/partials/result.html", context)
 
 
 class TestView(View):
